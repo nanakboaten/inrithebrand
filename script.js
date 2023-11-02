97441291d1b2fafb6c41fcc6c65ba13f808afd44
boat <134570683+nanakboaten@users.noreply.github.com>
2023-11-02 16:23:19 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggleSidebar() {
+    var sidebar = document.querySelector('.sidebar');
+    sidebar.classList.toggle('open');
+
+    var sidebarToggle = document.querySelector('.sidebar-toggle');
+    sidebarToggle.classList.toggle('open');
+}
